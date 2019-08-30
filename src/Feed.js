c803a0e8a64b559428b4dcfee58d6747bde703b7
buzz <buzz@users.noreply.github.com>
2019-08-30 23:12:23 +0200

    Initial

---
+++ src/Feed.js
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import Parser from 'rss-parser'
+
+import css from './Feed.sass'
+
+const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/'
+
+const parser = new Parser()
+
+const Feed = ({ url }) => {
+  const [feed, setFeed] = useState({
+    title: 'Loading…',
+    items: [],
+  })
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await parser.parseURL(CORS_PROXY + url)
+      setFeed(data)
+      console.log(data)
+      data.items.forEach((item) => {
+        console.log(`${item.title}:${item.link}`)
+        console.log(item)
+      })
+      console.log('-----------------------------------------')
+    }
+    fetchData()
+  }, [url])
+
+  const parsedUrl = new URL(url)
+
+  const feedIcon = feed.image
+    ? (
+      <a
+        className={classNames('nondraggable', css.feedIcon)}
+        href={feed.link}
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <img
+          alt={feed.image.title || feed.title}
+          src={`https://favicon.keeweb.info/${parsedUrl.hostname}`}
+          title={feed.image.title || feed.title}
+        />
+      </a>
+    )
+    : null
+
+  return (
+    <div className={css.feed}>
+      <div className={css.feedHeader}>
+        {feedIcon}
+        <h2 className="nondraggable">
+          <a
+            href={feed.link}
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            {feed.title}
+          </a>
+        </h2>
+      </div>
+      <div className={css.feedList}>
+        <ul>
+          {feed.items.map((item) => (
+            <li
+              className={classNames('nondraggable', css.feedItem)}
+              key={item.guid || item.id}
+            >
+              <a
+                href={item.link}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                {item.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+Feed.propTypes = {
+  url: PropTypes.string.isRequired,
+}
+
+export default Feed
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './index.sass'
+import 'react-grid-layout/css/styles.css'
+import 'react-resizable/css/styles.css'
+
+import App from './App'
+
+ReactDOM.render(<App />, document.getElementById('root'))
