2c29e1f806731a4b4883989920998c1546669d0a
buzz <buzz@users.noreply.github.com>
2019-08-31 21:18:32 +0200

    remove addSaga

--- src/sagas/feed/index.js
+++ src/sagas/feed/index.js
@@ -1,12 +1,10 @@
 import { takeEvery } from 'redux-saga/effects'
 
-import addFeedSaga from './addFeed'
 import editFeedSaga from './editFeed'
 import loadFeedSaga from './loadFeed'
 import { actionTypes as feedActionTypes } from '../../store/actions/feed'
 
 export default [
-  takeEvery(feedActionTypes.ADD_FEED, addFeedSaga),
   takeEvery(feedActionTypes.EDIT_FEED, editFeedSaga),
   takeEvery(feedActionTypes.LOAD_FEED, loadFeedSaga),
 ]
