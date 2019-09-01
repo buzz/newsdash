fe4309163bb4e01735aaf43aab1aeb49ddc2a7c1
buzz <buzz@users.noreply.github.com>
2019-09-02 00:04:46 +0200

    Add slide-in animation for new feed items

--- src/components/NewsGrid/Feed/body/List/index.js
+++ src/components/NewsGrid/Feed/body/List/index.js
@@ -1,14 +1,41 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 import css from './List.sass'
+import itemTransition from './itemTransition.sass'
 import { feedItemType } from '../../../../../propTypes'
 import Item from './Item'
 
+const { transitionSpeed, ...transitionClassNames } = itemTransition
+
+/* eslint no-param-reassign:
+   ["error", { "props": true, "ignorePropertyModificationsFor": ["node"] }] */
+const onEnter = (node) => {
+  node.style.marginTop = `-${node.offsetHeight}px`
+}
+const onEntering = (node) => {
+  node.style.marginTop = ''
+}
+
 const List = ({ items }) => (
-  <ul className={css.feedList}>
-    {items.map((item) => <Item item={item} key={item.id} />)}
-  </ul>
+  <TransitionGroup className={css.feedList} component="ul">
+    {
+      items.map(
+        (item) => (
+          <CSSTransition
+            classNames={{ ...transitionClassNames }}
+            key={item.id}
+            timeout={parseInt(transitionSpeed.slice(0, -2), 10)}
+            onEnter={onEnter}
+            onEntering={onEntering}
+          >
+            <Item item={item} />
+          </CSSTransition>
+        )
+      )
+    }
+  </TransitionGroup>
 )
 
 List.propTypes = {
