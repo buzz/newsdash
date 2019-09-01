d28b45212dc343749b414c7b78436687aaffc730
buzz <buzz@users.noreply.github.com>
2019-09-01 14:42:02 +0200

    Prune all items when changing a feed URL

--- src/sagas/feed/editFeed.js
+++ src/sagas/feed/editFeed.js
@@ -1,12 +1,14 @@
 import { put } from 'redux-saga/effects'
 
 import { loadFeed } from '../../store/actions/feed'
+import { prune } from '../../store/actions/feedItem'
 
 export default function* editFeed({ id, feed, prevFeed }) {
   if (
     feed.url !== ''
     && feed.url !== prevFeed.url
   ) {
+    yield put(prune(id))
     yield put(loadFeed(id, feed.url, feed.lastFetched))
   }
 }
