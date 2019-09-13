a587a96aad96dd939ddd6cbc5b67dd9ab4c945ac
buzz <buzz@users.noreply.github.com>
2019-09-13 14:30:38 +0200

    make feed URL editable

---
+++ src/sagas/feed/editFeedSaga.js
@@ -0,0 +1,11 @@
+import { call, put } from 'redux-saga/effects'
+
+import { loadFeed } from '../../store/actions/feed'
+
+export default function* fetchFeedsSaga({ id, attrs }) {
+  const attrsKeys = yield call([Object, Object.keys], attrs)
+  const urlChanged = yield call([attrsKeys, attrsKeys.includes], 'url')
+  if (urlChanged) {
+    yield put(loadFeed(id, attrs.url))
+  }
+}
