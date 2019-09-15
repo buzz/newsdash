7cb9fb1c3a1a97fa8961cfcc3c21c6c7f52c69f9
buzz <buzz@users.noreply.github.com>
2019-09-15 22:53:50 +0200

    load feeds from API proxy (#31)

--- packages/client/src/sagas/feed/loadFeedSaga.js
+++ packages/client/src/sagas/feed/loadFeedSaga.js
@@ -2,28 +2,17 @@ import { call, put, select } from 'redux-saga/effects'
 import Parser from 'rss-parser'
 
 import feedSelectors from 'newsdash/store/selectors/feed'
-import { loadFeedFailure, loadFeedSuccess, setUseCorsProxy } from 'newsdash/store/actions/feed'
-import getApp from 'newsdash/store/selectors/app'
+import { loadFeedFailure, loadFeedSuccess } from 'newsdash/store/actions/feed'
 
 const parser = new Parser()
 
-// first time try to fetch feed directly and remember if CORS proxy is necessary
 export default function* loadFeedSaga({ id, url }) {
   const feed = yield select(feedSelectors.getFeed, id)
-  const fetchUrl = url || feed.url
+  const encodedUrl = yield call(encodeURIComponent, url || feed.url)
+  const fetchUrl = `api/fetch-feed/${encodedUrl}`
 
-  if (!feed.useCorsProxy) {
-    try {
-      yield put(loadFeedSuccess(id, yield call([parser, parser.parseURL], fetchUrl)))
-      return
-    } catch {
-      yield put(setUseCorsProxy(id))
-    }
-  }
-
-  const { corsProxy } = yield select(getApp)
   try {
-    yield put(loadFeedSuccess(id, yield call([parser, parser.parseURL], `${corsProxy}${fetchUrl}`)))
+    yield put(loadFeedSuccess(id, yield call([parser, parser.parseURL], fetchUrl)))
   } catch (e) {
     yield put(loadFeedFailure(id, e.message))
   }
