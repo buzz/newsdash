0e64572bafef869f5e7f407afb013a840696fb62
buzz <buzz@users.noreply.github.com>
2019-09-20 13:41:47 +0200

    Add NoTextTile

---
+++ packages/client/src/components/Feed/Tiles/Tile/Overlay/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { feedItemType } from 'newsdash/components/propTypes'
+import Date from 'newsdash/components/Feed/Date'
+import css from './Overlay.sass'
+
+const Overlay = ({ className, item }) => {
+  const date = item.date
+    ? <Date className={css.date} date={item.date} />
+    : null
+  const content = item.content
+    ? (
+      <p className={css.content}>
+        {item.content}
+      </p>
+    )
+    : null
+
+  return (
+    <div className={className}>
+      <div className={css.caption}>
+        <h2 className={css.title}>{item.title}</h2>
+        {date}
+      </div>
+      {content}
+    </div>
+  )
+}
+
+Overlay.propTypes = {
+  className: PropTypes.string.isRequired,
+  date: PropTypes.number.isRequired,
+  item: feedItemType.isRequired,
+}
+
+export default Overlay
