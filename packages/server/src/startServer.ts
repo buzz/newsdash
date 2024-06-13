34d4f2dc48965b8c926bf6b84774cc66ad956056
buzz <buzz@users.noreply.github.com>
2024-06-13 21:55:12 +0200

    server: add pretty log printing for dev

--- packages/server/src/startServer.ts
+++ packages/server/src/startServer.ts
@@ -4,9 +4,22 @@ import { DEFAULT_PORT } from '#constants'
 
 import apiPlugin from './api/api.js'
 
-const fastify = Fastify({
-  logger: process.env.NODE_ENV !== 'production',
-})
+const logger =
+  process.env.NODE_ENV === 'production'
+    ? {
+        level: 'warn',
+      }
+    : {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            translateTime: 'HH:MM:ss Z',
+            ignore: 'pid,hostname',
+          },
+        },
+      }
+
+const fastify = Fastify({ logger })
 
 await fastify.register(apiPlugin, { prefix: '/api' })
 
