33716c83938caa79db47899463d05c969a652476
buzz <buzz@users.noreply.github.com>
2024-06-15 18:43:34 +0200

    client: improve feed item popover

--- packages/client/src/ui/components/Feed/Popover/Popover.tsx
+++ packages/client/src/ui/components/Feed/Popover/Popover.tsx
@@ -1,9 +1,15 @@
-import { AspectRatio, Group, HoverCard, Image, Text } from '@mantine/core'
-import type { ReactNode } from 'react'
+import { AspectRatio, Group, Image, Popover as MantinePopover, Text } from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
+import { type ReactNode, useState } from 'react'
 
 import classes from './Popover.module.css'
 
-function Popover({ children, content, imageUrl, title }: HoverCardProps) {
+const OPEN_DELAY = 500
+
+function Popover({ children, content, imageUrl, title }: PopoverProps) {
+  const [opened, { close, open }] = useDisclosure(false)
+  const [openTimeout, setOpenTimeout] = useState<number | undefined>()
+
   const image = imageUrl ? (
     <AspectRatio className={classes.image} ratio={20 / 15} w={300}>
       <Image src={imageUrl} />
@@ -12,10 +18,36 @@ function Popover({ children, content, imageUrl, title }: HoverCardProps) {
 
   const contentText = content ? <Text size="sm">{content}</Text> : null
 
+  const onMouseEnter = () => {
+    setOpenTimeout(setTimeout(open, OPEN_DELAY))
+  }
+
+  const onMouseLeave = () => {
+    if (openTimeout) {
+      clearTimeout(openTimeout)
+    }
+    close()
+  }
+
   return (
-    <HoverCard arrowSize={10} openDelay={500} shadow="md" width={600} withArrow>
-      <HoverCard.Target>{children}</HoverCard.Target>
-      <HoverCard.Dropdown className={classes.popover}>
+    <MantinePopover
+      arrowSize={10}
+      keepMounted={false}
+      opened={opened}
+      shadow="md"
+      width={600}
+      withArrow
+    >
+      <MantinePopover.Target>
+        <div
+          className={classes.targetWrapper}
+          onMouseEnter={onMouseEnter}
+          onMouseLeave={onMouseLeave}
+        >
+          {children}
+        </div>
+      </MantinePopover.Target>
+      <MantinePopover.Dropdown className={classes.popover}>
         <Group className={classes.flexWrap} wrap="nowrap">
           {image}
           <div>
@@ -25,12 +57,12 @@ function Popover({ children, content, imageUrl, title }: HoverCardProps) {
             {contentText}
           </div>
         </Group>
-      </HoverCard.Dropdown>
-    </HoverCard>
+      </MantinePopover.Dropdown>
+    </MantinePopover>
   )
 }
 
-interface HoverCardProps {
+interface PopoverProps {
   children: ReactNode
   content?: string
   imageUrl?: string
