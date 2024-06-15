7bff9b8323282682d93c529cc2a2da7ab701c0e7
buzz <buzz@users.noreply.github.com>
2024-06-15 17:15:28 +0200

    client: add overlayscrollbars

--- packages/client/src/ui/components/Feed/Feed.tsx
+++ packages/client/src/ui/components/Feed/Feed.tsx
@@ -1,7 +1,6 @@
-import cx from 'clsx'
-import { useState } from 'react'
+import 'overlayscrollbars/styles/overlayscrollbars.css'
+
 import AutoSizer from 'react-virtualized-auto-sizer'
-import { FixedSizeList } from 'react-window'
 
 import { selectByTabId } from '#store/slices/feedItems/selectors'
 import { useSelector } from '#ui/hooks/store'
@@ -9,6 +8,7 @@ import type { RootState } from '#store/types'
 import type { CustomTabData } from '#types/layout'
 
 import DetailFeedItem from './DetailFeedItem/DetailFeedItem'
+import WindowedScroller from './WindowedScroller'
 
 import classes from './Feed.module.css'
 
@@ -17,37 +17,14 @@ const ITEM_HEIGHT = 92
 function Feed({ tab }: FeedProps) {
   const selector = (state: RootState) => selectByTabId(state, tab.id ?? '')
   const feedItems = useSelector(selector)
-  const [isAt, setIsAt] = useState<'top' | 'bottom' | undefined>('top')
 
   return (
-    <div
-      className={cx(classes.wrapper, {
-        [classes.atTop]: isAt === 'top',
-        [classes.atBottom]: isAt === 'bottom',
-      })}
-    >
-      <AutoSizer>
-        {({ height, width }) => (
-          <FixedSizeList
-            className={classes.scroller}
-            height={height}
-            width={width}
-            itemCount={feedItems.length}
-            itemData={feedItems}
-            itemSize={ITEM_HEIGHT}
-            onScroll={({ scrollOffset }) => {
-              if (scrollOffset < 2) {
-                setIsAt('top')
-              } else if (scrollOffset + height > ITEM_HEIGHT * feedItems.length - 2) {
-                setIsAt('bottom')
-              } else {
-                setIsAt(undefined)
-              }
-            }}
-            overscanCount={1}
-          >
+    <div className={classes.wrapper}>
+      <AutoSizer disableWidth>
+        {({ height }) => (
+          <WindowedScroller height={height} itemHeight={ITEM_HEIGHT} items={feedItems}>
             {DetailFeedItem}
-          </FixedSizeList>
+          </WindowedScroller>
         )}
       </AutoSizer>
     </div>
---
+++ packages/client/src/ui/components/Feed/WindowedScroller.tsx
@@ -0,0 +1,84 @@
+import cx from 'clsx'
+import { useOverlayScrollbars, type UseOverlayScrollbarsParams } from 'overlayscrollbars-react'
+import { useEffect, useRef, useState } from 'react'
+import { FixedSizeList } from 'react-window'
+import type { ComponentType } from 'react'
+import type { ListChildComponentProps } from 'react-window'
+
+import classes from './Feed.module.css'
+
+const overlayScrollbarsParams: UseOverlayScrollbarsParams = {
+  defer: true,
+  options: {
+    overflow: {
+      x: 'hidden',
+      y: 'scroll',
+    },
+    scrollbars: {
+      autoHide: 'leave',
+      autoHideDelay: 500,
+      theme: classes.osTheme,
+    },
+    update: { elementEvents: null },
+  },
+}
+
+function WindowedScroller<T>({ children, height, itemHeight, items }: WindowedScrollerProps<T>) {
+  const [isAt, setIsAt] = useState<'top' | 'bottom' | undefined>('top')
+  const rootRef = useRef<HTMLDivElement>(null)
+  const outerRef = useRef<HTMLDivElement>(null)
+  const [initialize, osInstance] = useOverlayScrollbars(overlayScrollbarsParams)
+
+  useEffect(() => {
+    if (rootRef.current && outerRef.current) {
+      initialize({
+        target: rootRef.current,
+        elements: {
+          viewport: outerRef.current,
+        },
+      })
+    }
+    return () => osInstance()?.destroy()
+  }, [initialize, osInstance])
+
+  return (
+    <div
+      className={cx(classes.scroller, {
+        [classes.atTop]: isAt === 'top',
+        [classes.atBottom]: isAt === 'bottom',
+      })}
+      ref={rootRef}
+    >
+      <FixedSizeList
+        className={classes.list}
+        height={height}
+        width="100%"
+        itemCount={items.length}
+        itemData={items}
+        itemSize={itemHeight}
+        outerRef={outerRef}
+        onScroll={({ scrollOffset }) => {
+          if (scrollOffset < 2) {
+            setIsAt('top')
+          } else if (scrollOffset + height > itemHeight * items.length - 2) {
+            setIsAt('bottom')
+          } else {
+            setIsAt(undefined)
+          }
+        }}
+        overscanCount={1}
+      >
+        {children}
+      </FixedSizeList>
+    </div>
+  )
+}
+
+interface WindowedScrollerProps<T> {
+  children: ComponentType<ListChildComponentProps<T[]>>
+  height: number
+  itemHeight: number
+  items: T[]
+}
+
+export default WindowedScroller
