c4f975369662875b78252a44df9d0afeef0818dc
buzz <buzz@users.noreply.github.com>
2024-06-23 03:37:05 +0200

    client: replace overlayscrollbars with simplebar

--- packages/client/src/ui/components/Feed/WindowedScroller.tsx
+++ packages/client/src/ui/components/Feed/WindowedScroller.tsx
@@ -1,9 +1,10 @@
-import 'overlayscrollbars/styles/overlayscrollbars.css'
+import 'simplebar-react/dist/simplebar.min.css'
+import './simplebar.css'
 
 import cx from 'clsx'
-import { useOverlayScrollbars, type UseOverlayScrollbarsParams } from 'overlayscrollbars-react'
-import { useEffect, useRef, useState } from 'react'
+import { useState } from 'react'
 import { FixedSizeList } from 'react-window'
+import SimpleBarReact from 'simplebar-react'
 
 import type { Display } from '@newsdash/schema'
 
@@ -12,22 +13,6 @@ import type { FeedItem } from '#types/feed'
 import FeedItemRow from './FeedItemRow/FeedItemRow'
 
 import classes from './Feed.module.css'
-
-const overlayScrollbarsParams: UseOverlayScrollbarsParams = {
-  defer: true,
-  options: {
-    overflow: {
-      x: 'hidden',
-      y: 'scroll',
-    },
-    scrollbars: {
-      autoHide: 'leave',
-      autoHideDelay: 500,
-      theme: classes.osTheme,
-    },
-    update: { elementEvents: null },
-  },
-}
 
 function WindowedScroller({
   display,
@@ -40,51 +25,39 @@ function WindowedScroller({
   overscanCount = 1,
 }: WindowedScrollerProps) {
   const [isAt, setIsAt] = useState<'top' | 'bottom' | undefined>('top')
-  const rootRef = useRef<HTMLDivElement>(null)
-  const outerRef = useRef<HTMLDivElement>(null)
-  const [initialize, osInstance] = useOverlayScrollbars(overlayScrollbarsParams)
-
-  useEffect(() => {
-    if (rootRef.current && outerRef.current) {
-      initialize({
-        target: rootRef.current,
-        elements: {
-          viewport: outerRef.current,
-        },
-      })
-    }
-  }, [initialize, osInstance])
-
   const columnCount = Math.max(1, Math.floor(width / minWidth))
   const rowCount = Math.ceil(items.length / columnCount)
 
   return (
-    <div ref={rootRef}>
-      <FixedSizeList
-        className={cx(classes.list, {
-          [classes.atTop]: isAt === 'top',
-          [classes.atBottom]: isAt === 'bottom',
-        })}
-        height={height}
-        width={width}
-        itemCount={rowCount}
-        itemSize={rowHeight}
-        itemData={{ items, display, language, columnCount }}
-        outerRef={outerRef}
-        onScroll={({ scrollOffset }) => {
-          if (scrollOffset < 2) {
-            setIsAt('top')
-          } else if (scrollOffset + height > rowHeight * rowCount - 2) {
-            setIsAt('bottom')
-          } else {
-            setIsAt(undefined)
-          }
-        }}
-        overscanCount={overscanCount}
-      >
-        {FeedItemRow}
-      </FixedSizeList>
-    </div>
+    <SimpleBarReact clickOnTrack={false} style={{ height: `${height}px` }}>
+      {({ scrollableNodeRef, contentNodeRef }) => (
+        <FixedSizeList
+          className={cx(classes.list, {
+            [classes.atTop]: isAt === 'top',
+            [classes.atBottom]: isAt === 'bottom',
+          })}
+          height={height}
+          width={width}
+          itemCount={rowCount}
+          itemSize={rowHeight}
+          itemData={{ items, display, language, columnCount }}
+          outerRef={scrollableNodeRef}
+          innerRef={contentNodeRef}
+          onScroll={({ scrollOffset }) => {
+            if (scrollOffset < 2) {
+              setIsAt('top')
+            } else if (scrollOffset + height > rowHeight * rowCount - 2) {
+              setIsAt('bottom')
+            } else {
+              setIsAt(undefined)
+            }
+          }}
+          overscanCount={overscanCount}
+        >
+          {FeedItemRow}
+        </FixedSizeList>
+      )}
+    </SimpleBarReact>
   )
 }
 
