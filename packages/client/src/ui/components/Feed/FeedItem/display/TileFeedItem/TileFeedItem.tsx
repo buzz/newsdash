c1944cdfc3bf57667f4cf40bc93239ccd2745100
buzz <buzz@users.noreply.github.com>
2024-07-13 01:49:50 +0200

    client: slide-up overlay text

--- packages/client/src/ui/components/Feed/FeedItem/display/TileFeedItem/TileFeedItem.tsx
+++ packages/client/src/ui/components/Feed/FeedItem/display/TileFeedItem/TileFeedItem.tsx
@@ -1,11 +1,17 @@
+import cx from 'clsx'
+
 import type { FeedItemComponentProps } from '#ui/components/Feed/FeedItem/types'
 
 import classes from './TileFeedItem.module.css'
 
-function TileFeedItem({ feedItem, imageUrl }: FeedItemComponentProps) {
+function TileFeedItem({ feedItem, imageUrl, language }: FeedItemComponentProps) {
   return (
-    <div className={classes.wrapper}>
+    <div className={cx(classes.wrapper, { [classes.slideUp]: Boolean(feedItem.content) })}>
       <img alt={feedItem.title} className={classes.image} src={imageUrl} />
+      <div className={classes.textOverlay} lang={language}>
+        <div className={classes.title}>{feedItem.title}</div>
+        <div className={classes.content}>{feedItem.content}</div>
+      </div>
     </div>
   )
 }
