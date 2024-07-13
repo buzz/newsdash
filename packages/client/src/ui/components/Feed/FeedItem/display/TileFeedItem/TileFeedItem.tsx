027a22c4c7570e966b6d552b22c2f522622ba98f
buzz <buzz@users.noreply.github.com>
2024-07-13 13:28:43 +0200

    client: add `TimeAgoBatch` to `TileFeedItem`

--- packages/client/src/ui/components/Feed/FeedItem/display/TileFeedItem/TileFeedItem.tsx
+++ packages/client/src/ui/components/Feed/FeedItem/display/TileFeedItem/TileFeedItem.tsx
@@ -1,5 +1,6 @@
 import cx from 'clsx'
 
+import TimeAgoBadge from '#ui/components/Feed/FeedItem/TimeAgoBadge/TimeAgoBadge'
 import type { FeedItemComponentProps } from '#ui/components/Feed/FeedItem/types'
 
 import classes from './TileFeedItem.module.css'
@@ -9,7 +10,10 @@ function TileFeedItem({ feedItem, imageUrl, language }: FeedItemComponentProps) 
     <div className={cx(classes.wrapper, { [classes.slideUp]: Boolean(feedItem.content) })}>
       <img alt={feedItem.title} className={classes.image} src={imageUrl} />
       <div className={classes.textOverlay} lang={language}>
-        <div className={classes.title}>{feedItem.title}</div>
+        <div className={classes.title}>
+          <TimeAgoBadge className={classes.badge} feedItem={feedItem} />
+          {feedItem.title}
+        </div>
         <div className={classes.content}>{feedItem.content}</div>
       </div>
     </div>
