ed9826917e13c193413a525c045969e75b87c56f
buzz <buzz@users.noreply.github.com>
2024-07-17 17:51:51 +0200

    client: only persist layout if no edit tab is open

--- packages/client/src/store/middlewares/layoutListenerMiddleware/effects/persistLayoutEffect.ts
+++ packages/client/src/store/middlewares/layoutListenerMiddleware/effects/persistLayoutEffect.ts
@@ -9,6 +9,7 @@ import { selectPersistLayout } from '#store/slices/api/selectors'
 import { updateLayout } from '#store/slices/layout/actions'
 import { addPanel, removePanel, updatePanel } from '#store/slices/layout/entities/panels/actions'
 import { addTab, editTab, removeTab } from '#store/slices/layout/entities/tabs/actions'
+import { selectEditTabs } from '#store/slices/layout/entities/tabs/selectors'
 import type { AppStartListening } from '#store/middlewares/types'
 
 const PERSIST_DELAY = 5000
@@ -23,14 +24,15 @@ function persistLayoutEffect(startListening: AppStartListening) {
       await debounce(listenerApi, PERSIST_DELAY)
 
       const state = listenerApi.getState()
-      const persistLayout = selectPersistLayout(state)
 
-      // TODO: don't persist while feed settings is open
-
-      // Only persist if layout actually changed
-      if (!isEqual(persistLayout, lastPersistLayout)) {
-        await listenerApi.dispatch(layoutApi.endpoints.persistLayout.initiate(persistLayout))
-        lastPersistLayout = persistLayout
+      // Only persist we're not editing
+      if (selectEditTabs(state).length === 0) {
+        // Only persist if layout actually changed
+        const persistLayout = selectPersistLayout(state)
+        if (!isEqual(persistLayout, lastPersistLayout)) {
+          await listenerApi.dispatch(layoutApi.endpoints.persistLayout.initiate(persistLayout))
+          lastPersistLayout = persistLayout
+        }
       }
     },
   })
