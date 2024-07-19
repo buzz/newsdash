bef3569e405ae6e1f92e8442035cba204f8543aa
buzz <buzz@users.noreply.github.com>
2024-07-19 04:03:30 +0200

    client: update `vite.config.ts`

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,13 +1,11 @@
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
-// import { ViteFaviconsPlugin as favicons } from 'vite-plugin-favicon2'
 import svgr from 'vite-plugin-svgr'
 
-// import pkgInfo from '../../package.json' with { type: 'json' }
-
 export default defineConfig({
-  envDir: '../..',
-  envPrefix: 'NEWSDASH_', // Exposed to client
+  build: {
+    target: ['chrome126', 'firefox128'],
+  },
   server: {
     port: 3000,
     proxy: { '/api': 'http://localhost:3001' },
@@ -17,17 +15,5 @@ export default defineConfig({
     svgr({
       svgrOptions: { dimensions: false },
     }),
-    // favicons({
-    //   favicons: {
-    //     appName: pkgInfo.name,
-    //     appDescription: pkgInfo.description,
-    //     version: pkgInfo.version,
-    //     icons: {
-    //       appleIcon: false,
-    //       appleStartup: false,
-    //     },
-    //   },
-    //   logo: '../../artwork/favicon.svg',
-    // }),
   ],
 })
